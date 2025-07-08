fe21585b7fae59ae726a3ad061699ec2dbcba0bb
Sneha Bangera <snehabangera05@gmail.com>
2025-07-08 20:06:23 +0530

    abt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import gsap from 'gsap/all'
 import { SplitText, ScrollTrigger } from 'gsap/all'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Cocktails from './components/Cocktails'
+import About from './components/About'
+import Art from './components/Art'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -11,6 +14,9 @@ const App = () => {
     <main>
       <Navbar/>
       <Hero/>
+      <Cocktails/>
+      <About/>
+      <Art/>
     </main>
   )
 }
